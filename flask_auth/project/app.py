ab915de61497dfbde89dc9eed5134e6dd4c929c0
Dmitry R <dmr.main@outlook.com>
2023-03-12 16:07:17 +0300

    generic exception handler added

--- flask_auth/project/app.py
+++ flask_auth/project/app.py
@@ -1,23 +1,16 @@
 """App module."""
 
 from datetime import timedelta
-
-from flask import Flask, request
-
-from authlib.integrations.flask_client import OAuth
-
+from flask import Flask, request, json
 from flasgger import Swagger
-
 from flask_migrate import Migrate
-
 from opentelemetry.instrumentation.flask import FlaskInstrumentor
+from werkzeug.exceptions import HTTPException
 
 from flask_auth.project.db import db, init_db
 from flask_auth.project.settings import app_settings
-
 # Import models to create in the db.
 from flask_auth.project.models import Role, User  # noqa: F401
-
 from flask_auth.project.utils.configure_tracer import configure_tracer
 
 
@@ -34,9 +27,6 @@ def before_request():
     if not request_id:
         raise RuntimeError("request id is required")
 
-
-app = Flask(__name__)
-swagger = Swagger(app)
 
 app.config["SECRET_KEY"] = app_settings.SECRET_KEY
 app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(days=1)
@@ -58,6 +48,21 @@ from flask_auth.project.api.v1.roles.roles import roles_blueprint  # noqa: E402,
 app.register_blueprint(roles_blueprint, url_prefix="/api/v1/roles")
 from flask_auth.project.cli.cli import cli_blueprint  # noqa: E402, I202
 app.register_blueprint(cli_blueprint, cli_group=None)
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+    """Return JSON instead of HTML for HTTP errors."""
+    # start with the correct headers and status code from the error
+    response = e.get_response()
+    # replace the body with JSON
+    response.data = json.dumps({
+        "code": e.code,
+        "name": e.name,
+        "description": e.description,
+    })
+    response.content_type = "application/json"
+    return response
 
 
 if __name__ == "__main__":
